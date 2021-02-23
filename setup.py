ec52700f179ed793004928e538bb96eba0632dcf
Ildar <ildar0364@gmail.com>
2021-02-23 19:29:09 +0400

    0.5.19

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
 setup(name='mpets',
-      version='0.5.18',
+      version='0.5.19',
       description='API for game AmazingPets',
       packages=['mpets', 'mpets.utils', 'mpets.models'],
       author_email='wilidon@bk.ru',
+      install_requires=[
+          'python-box[all]',
+          'aiohttp',
+          'BeautifulSoup4',
+          'lxml',
+          'aiohttp-socks'
+      ],
       zip_safe=False)
