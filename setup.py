4d17c707bac2448e252a97761e89f5d9faf49b24
Ildar <ildar0364@gmail.com>
2021-04-03 21:52:49 +0400

    0.5.30

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mpets',
-      version='0.5.28',
+      version='0.5.30',
       description='API for game AmazingPets',
       packages=['mpets', 'mpets.utils', 'mpets.models'],
       author_email='wilidon@bk.ru',
